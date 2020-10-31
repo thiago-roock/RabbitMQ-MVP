f56121857a0cc21438c96208a71dea75e8635dd3
Thiago de Melo Lima <56129760+RDPodcasting@users.noreply.github.com>
2020-10-31 13:50:27 -0300

    criação da logica de produtor e consumer

--- RabbitmqDemo/Processador-Votos/Program.cs
+++ RabbitmqDemo/Processador-Votos/Program.cs
@@ -1,4 +1,7 @@
-﻿using System;
+﻿using RabbitMQ.Client;
+using RabbitMQ.Client.Events;
+using System;
+using System.Text;
 
 namespace Processador_Votos
 {
@@ -6,7 +9,31 @@ namespace Processador_Votos
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            var factory = new ConnectionFactory() { HostName = "localhost" };
+            using var connection = factory.CreateConnection();
+            using var channel = connection.CreateModel();
+            
+            channel.QueueDeclare(queue: "votos",
+                                 durable: false,
+                                 exclusive: false,
+                                 autoDelete: false,
+                                 arguments: null);
+
+            var consumer = new EventingBasicConsumer(channel);
+            consumer.Received += (model, ea) =>
+            {
+                var body = ea.Body.ToArray();
+                var message = Encoding.UTF8.GetString(body);
+                Console.WriteLine(" [x] recebido 1 voto em {0}", message);
+            };
+            channel.BasicConsume(queue: "votos",
+                                 autoAck: true,
+                                 consumer: consumer);
+
+            Console.WriteLine(" Pressione [enter] para sair.");
+            Console.ReadLine();
+        
         }
     }
 }
+
--- RabbitmqDemo/Simulador-Votos/Program.cs
+++ RabbitmqDemo/Simulador-Votos/Program.cs
@@ -1,4 +1,6 @@
 ﻿using System;
+using RabbitMQ.Client;
+using System.Text;
 
 namespace Simulador_Votos
 {
@@ -6,7 +8,29 @@ namespace Simulador_Votos
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            var factory = new ConnectionFactory() { HostName = "localhost" };
+            using var connection = factory.CreateConnection();
+            using var channel = connection.CreateModel();
+            
+            channel.QueueDeclare(queue: "votos",
+                                 durable: false,
+                                 exclusive: false,
+                                 autoDelete: false,
+                                 arguments: null);
+            string[] participantes = {"Thiago","Jaqueline"};
+            string message = $@"{participantes[0]}";
+            var body = Encoding.UTF8.GetBytes(message);
+
+            channel.BasicPublish(exchange: "",
+                                 routingKey: "votos",
+                                 basicProperties: null,
+                                 body: body);
+            Console.WriteLine(" [x] enviando 1 voto em {0}", message);
+        
+
+            Console.WriteLine(" Pressione [enter] para sair.");
+            Console.ReadLine();
+    
         }
     }
 }
